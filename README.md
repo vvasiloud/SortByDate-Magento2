9e242a70072717bb959e5d8bcfeb2d4dfbb44443
vvasiloud <vvasiloudis@gmail.com>
2016-09-07 21:39:29 +0300

    Module commit

---
+++ README.md
@@ -0,0 +1,51 @@
+Sort By Date
+==================
+
+Extend catalogue ordering with product creation date.
+
+<h2>Install:</h2>
+-Install by Composer : You can install the module by Composer by running the following comange in your server
+
+composer require vvasiloud/sortbydate
+
+-Install by uploading files:
+
+Download packed as ".zip" file and unzip the extension to {{MAGENTO-ROOT}}/app/code/
+
+Alternatively clone this repository by the following commands:
+
+Use SSH: 
+```bash
+git clone git@github.com:vvasiloud/SortByDate-Magento2.git
+```
+
+Use HTTPS: 
+```bash
+git clone https://github.com/vvasiloud/SortByDate-Magento2.git
+```
+
+After install run the following command using SSH:
+```bash
+php bin/magento setup:upgrade
+```
+
+How to use?
+-------------
+
+1. Just install and it's ready to use :).
+
+Support
+-------
+If you encounter any problems or bugs, please create an issue on [GitHub](https://github.com/vvasiloud/SortByDate-Magento2/issues).
+
+Contribution
+------------
+Any contribution is highly welcome. To provide any code open a [pull request on GitHub](https://github.com/vvasiloud/SortByDate-Magento2/pulls).
+
+Licence
+-------
+[GNU General Public License](http://www.gnu.org/licenses/)
+
+Copyright
+---------
+(c) 2016 vvasiloud
---
+++ Vvasiloud/SortByDate/Block/Catalog/Product/ProductList/Toolbar.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * A Magento 2 module named Vvasiloud/SortByDate
+ * Copyright (C) 2016  
+ * 
+ * This file is part of Vvasiloud/SortByDate.
+ * 
+ * Vvasiloud/SortByDate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+namespace Vvasiloud\SortByDate\Block\Catalog\Product\ProductList;
+
+use Magento\Catalog\Helper\Product\ProductList;
+use Magento\Catalog\Model\Product\ProductList\Toolbar as ToolbarModel;
+
+/**
+ * Product list toolbar
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ * @SuppressWarnings(PHPMD.TooManyFields)
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Toolbar extends \Magento\Catalog\Block\Product\ProductList\Toolbar
+{
+    /**
+     * Products collection
+     *
+     * @var \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+     */
+    protected $_collection = null;
+
+    /**
+     * List of available order fields
+     *
+     * @var array
+     */
+    protected $_availableOrder = null;
+
+    /**
+     * List of available view types
+     *
+     * @var array
+     */
+    protected $_availableMode = [];
+
+    /**
+     * Is enable View switcher
+     *
+     * @var bool
+     */
+    protected $_enableViewSwitcher = true;
+
+    /**
+     * Is Expanded
+     *
+     * @var bool
+     */
+    protected $_isExpanded = true;
+
+    /**
+     * Default Order field
+     *
+     * @var string
+     */
+    protected $_orderField = null;
+
+    /**
+     * Default direction
+     *
+     * @var string
+     */
+    protected $_direction = ProductList::DEFAULT_SORT_DIRECTION;
+
+    /**
+     * Default View mode
+     *
+     * @var string
+     */
+    protected $_viewMode = null;
+
+    /**
+     * @var bool $_paramsMemorizeAllowed
+     */
+    protected $_paramsMemorizeAllowed = true;
+
+    /**
+     * @var string
+     */
+    protected $_template = 'product/list/toolbar.phtml';
+
+    /**
+     * Catalog config
+     *
+     * @var \Magento\Catalog\Model\Config
+     */
+    protected $_catalogConfig;
+
+    /**
+     * Catalog session
+     *
+     * @var \Magento\Catalog\Model\Session
+     */
+    protected $_catalogSession;
+
+    /**
+     * @var ToolbarModel
+     */
+    protected $_toolbarModel;
+
+    /**
+     * @var ProductList
+     */
+    protected $_productListHelper;
+
+    /**
+     * @var \Magento\Framework\Url\EncoderInterface
+     */
+    protected $urlEncoder;
+
+    /**
+     * @var \Magento\Framework\Data\Helper\PostHelper
+     */
+    protected $_postDataHelper;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Catalog\Model\Session $catalogSession
+     * @param \Magento\Catalog\Model\Config $catalogConfig
+     * @param ToolbarModel $toolbarModel
+     * @param \Magento\Framework\Url\EncoderInterface $urlEncoder
+     * @param ProductList $productListHelper
+     * @param \Magento\Framework\Data\Helper\PostHelper $postDataHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\Session $catalogSession,
+        \Magento\Catalog\Model\Config $catalogConfig,
+        ToolbarModel $toolbarModel,
+        \Magento\Framework\Url\EncoderInterface $urlEncoder,
+        ProductList $productListHelper,
+        \Magento\Framework\Data\Helper\PostHelper $postDataHelper,
+        array $data = []
+    ) {
+        $this->_catalogSession = $catalogSession;
+        $this->_catalogConfig = $catalogConfig;
+        $this->_toolbarModel = $toolbarModel;
+        $this->urlEncoder = $urlEncoder;
+        $this->_productListHelper = $productListHelper;
+        $this->_postDataHelper = $postDataHelper;
+        parent::__construct($context, $data);
+    }
+
+ 
+    /**
+     * Set collection to pager
+     *
+     * @param \Magento\Framework\Data\Collection $collection
+     * @return $this
+     */
+    public function setCollection($collection)
+    {
+        $this->_collection = $collection;
+
+        $this->_collection->setCurPage($this->getCurrentPage());
+
+        // we need to set pagination only if passed value integer and more that 0
+        $limit = (int)$this->getLimit();
+        if ($limit) {
+            $this->_collection->setPageSize($limit);
+        }
+		// no need to pass created_at in a custom colletion
+		// since we pass it directly from the option value
+		
+		/*if ($this->getCurrentOrder()) {
+			switch ($this->getCurrentOrder()) {
+				case 'created_at':
+					if ( $this->getCurrentDirection() == 'desc' ) {
+						$this->_collection
+							->getSelect()
+							->order('e.created_at DESC');
+
+					} elseif ( $this->getCurrentDirection() == 'asc' ) {
+						$this->_collection
+							->getSelect()
+							->order('e.created_at ASC');
+					}
+					break;
+
+				default:
+					$this->_collection->setOrder($this->getCurrentOrder(), $this->getCurrentDirection());
+					break;
+			} */
+			
+			$this->_collection->setOrder($this->getCurrentOrder(), $this->getCurrentDirection());
+
+        return $this;
+    }
+
+}
